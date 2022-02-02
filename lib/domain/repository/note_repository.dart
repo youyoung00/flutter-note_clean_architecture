b3285f1cd7e1c7e2d1b6a085f80118a4724f719e
youyoung00 <you889@hanmail.net>
2022-02-02 14:45:02 +0900

    노트앱 : Sqflite 0.0.4 version -> Sqflite 라이브러리로 data 레이아웃- data_source, repository 구현

--- lib/domain/repository/note_repository.dart
+++ lib/domain/repository/note_repository.dart
@@ -3,7 +3,7 @@ import 'package:note_clean_architecture/domain/model/note.dart';
 abstract class NoteRepository {
   Future<List<Note>> getNotes();
 
-  Future<Note> getNoteById(int id);
+  Future<Note?> getNoteById(int id);
 
   Future<void> insertNote(Note note);
 
