73eea1224de97a9b0f506ea1563252aac9ad1c64
youyoung00 <you889@hanmail.net>
2022-02-03 13:35:03 +0900

    노트 앱 : Dependency Injection 0.1.1 version -> DI 디렉토리를 만들어서 객체간 의존성 관리 적용

---
+++ lib/di/provider_setup.dart
@@ -0,0 +1,29 @@
+import 'package:note_clean_architecture/data/data_source/note_db_helper.dart';
+import 'package:note_clean_architecture/data/repository/note_repository_impl.dart';
+import 'package:note_clean_architecture/domain/repository/note_repository.dart';
+import 'package:note_clean_architecture/presentation/add_edit_note/add_edit_note_view_model.dart';
+import 'package:note_clean_architecture/presentation/notes/notes_view_model.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:sqflite/sqflite.dart';
+
+Future<List<SingleChildWidget>> getProviders() async {
+  Database database = await openDatabase(
+    'notes_db',
+    version: 1,
+    onCreate: (db, version) async {
+      await db.execute(
+          'CREATE TABLE note(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, content TEXT, color INTEGER, timestamp INTEGER)');
+    },
+  );
+
+  NoteDbHelper noteDbHelper = NoteDbHelper(database);
+  NoteRepository repository = NoteRepositoryImpl(noteDbHelper);
+  NotesViewModel notesViewModel = NotesViewModel(repository);
+  AddEditNoteViewModel addEditNoteViewModel = AddEditNoteViewModel(repository);
+
+  return [
+    ChangeNotifierProvider(create: (_) => notesViewModel),
+    ChangeNotifierProvider(create: (_) => addEditNoteViewModel),
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:note_clean_architecture/di/provider_setup.dart';
 import 'package:note_clean_architecture/presentation/notes/notes_screen.dart';
 import 'package:note_clean_architecture/ui/colors.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // 플랫폼 채널의 위젯 바인딩을 보장.
+  WidgetsFlutterBinding.ensureInitialized();
+  final providers = await getProviders();
+  runApp(
+    MultiProvider(
+      providers: providers,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
